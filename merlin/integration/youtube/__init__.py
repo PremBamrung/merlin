d1eb39fa9d223072c3ec72c5d8d682193a5fbe9a
Prem Bamrung <39570590+PremBamrung@users.noreply.github.com>
2026-06-11 19:11:11 -0400

    refactor: remove legacy first-gen Streamlit app

--- merlin/integration/youtube/__init__.py
+++
@@ -1,8 +0,0 @@
-"""YouTube integration package for video processing and summarization."""
-
-from merlin.integration.youtube.service import YouTubeService
-
-# For backwards compatibility, expose the service class as YouTube
-YouTube = YouTubeService
-
-__all__ = ["YouTube", "YouTubeService"]
